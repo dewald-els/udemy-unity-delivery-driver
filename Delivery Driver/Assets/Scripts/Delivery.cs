fe1c5fd9900cd246a3919f7110ab01d2d4cad3ad
MrBeta <dewaldifels@gmail.com>
2022-07-06 12:04:14 +0200

    feat: Completed Section 2: Delivery Driver - Custom Tweaks: Controller accel and break, returns to normal speed after 1.5sec from bump/boost

--- Delivery Driver/Assets/Scripts/Delivery.cs
+++ Delivery Driver/Assets/Scripts/Delivery.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 packageCollectedColour;
+
+    private bool hasPackage = false;
+
+    [Header("Components")]
+    private SpriteRenderer sprite;
+
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision == null) return;
 
-        if (collision.tag == "Package")
+        if (collision.tag == "Package" && hasPackage == false)
         {
             Debug.Log("Package Collected");
+            hasPackage = true;
+            Destroy(collision.gameObject);
+            sprite.color = packageCollectedColour;
         }
 
-        if (collision.tag == "Customer")
+        if (collision.tag == "Customer" && hasPackage)
         {
             Debug.Log("Delivered Package to customer");
+            hasPackage = false;
+            Destroy(collision.gameObject);
+            sprite.color = new Color(1, 1, 1, 1);
         }
         
     }
--- Delivery Driver/Assets/Scripts/Player.cs
+++ Delivery Driver/Assets/Scripts/Player.cs
@@ -1,26 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField]
     private float steerSpeed = 1.0f;
     [SerializeField]
-    private float moveSpeed = 0.01f;
+    private float moveSpeed = 10.0f;
+    private float currentSpeed = 0;
+    [SerializeField]
+    private float slowSpeed = 5.0f;
+    [SerializeField]
+    private float boostSpeed = 15.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentSpeed = moveSpeed;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        // float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float moveAmount = 0f;
+        if (Input.GetButton("Fire1"))
+        {
+            moveAmount = 1f * currentSpeed * Time.deltaTime;
+        }
+        if (Input.GetButton("Fire2"))
+        {
+            moveAmount = -1f * currentSpeed * Time.deltaTime;
+        }
+        
         transform.Rotate(0,0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+
+    void RestoreSpeed()
+    {
+        currentSpeed = moveSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision == null) return;
+        currentSpeed = slowSpeed;
+        Task.Delay(2000).ContinueWith(tag => RestoreSpeed());
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision == null) return;
+
+        if (collision.tag == "SpeedBoost")
+        {
+            Debug.Log("Speed Boost!");
+            currentSpeed = boostSpeed;
+            Task.Delay(2000).ContinueWith(tag => RestoreSpeed());
+        }
     }
 }
